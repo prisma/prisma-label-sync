c255babeec4e3ac0e1a8dbf6ecd9aee35ff23447
maticzav <matic.zavadlal@gmail.com>
2018-12-09 00:29:51 +0100

    added prisma/prisma labels

--- src/labels/common.ts
+++ src/labels/common.ts
@@ -4,3 +4,11 @@ export const common = {
     color: '123456',
   },
 }
+
+export const colors = {
+  area: 'fbca04',
+  kind: 'c5def5',
+  status: 'a9c1e8',
+  rfc: 'b621ff',
+  bug: 'd93f0b',
+}
--- src/labels/repositories/prisma.ts
+++ src/labels/repositories/prisma.ts
@@ -1,3 +1,43 @@
+import { colors } from '../common'
+
 export const prisma = {
-  labels: {},
+  labels: {
+    'rfc/0-needs-spec': colors.rfc,
+    'rfc/1-draft': colors.rfc,
+    'rfc/2-accepted': colors.rfc,
+    'rfc/x-rejected': colors.rfc,
+    'bug/2-confirmed': colors.bug,
+    'bug/0-needs-info': {
+      color: colors.bug,
+      description: 'More information is needed for reproduction.',
+    },
+    'bug/1-repro-available': {
+      color: colors.bug,
+      description: 'A reproduction exists and needs to be confirmed.',
+    },
+    'kind/discussion': colors.kind,
+    'kind/duplicate': colors.kind,
+    'kind/feature': colors.kind,
+    'kind/feedback': colors.kind,
+    'kind/question': colors.kind,
+    'status/pr-welcome': colors.status,
+    'status/research-needed': colors.status,
+    'status/candidate': {
+      color: colors.status,
+      description:
+        'This issue will probably be considered soon for the roadmap',
+    },
+    'status/next': {
+      color: colors.status,
+      description: 'Will be addressed with the next major version of Prisma',
+    },
+    'status/on-hold': {
+      color: colors.status,
+      description: 'This issue is on hold.',
+    },
+    'status/stale': {
+      color: colors.status,
+      description: 'Marked as state by the GitHub stalebot',
+    },
+  },
 }
