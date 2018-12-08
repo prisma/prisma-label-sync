bac32cd43ece5968968d5860a568441e8c9c6fc1
maticzav <matic.zavadlal@gmail.com>
2018-12-08 23:52:15 +0100

    added prisma/prisma, prettier

--- src/labels/index.ts
+++ src/labels/index.ts
@@ -1,7 +1,9 @@
 import { Config } from 'label-sync-core'
-import { prismaAdminFeedback } from './repositories/prisma-admin-feedback';
+import { prismaAdminFeedback } from './repositories/prisma-admin-feedback'
+import { prisma } from './repositories/prisma'
 
 const config: Config = {
+  'prisma/prisma': prisma,
   'prisma/prisma-admin-feedback': prismaAdminFeedback,
 }
 
