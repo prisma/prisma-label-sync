754377422dbe67df931d3fe1449831639805e5ba
Johannes Schickling <schickling.j@gmail.com>
2019-01-06 15:15:21 +0100

    Update index.ts

--- src/labels/index.ts
+++ src/labels/index.ts
@@ -3,6 +3,7 @@ import { prismaAdminFeedback } from './repositories/prisma-admin-feedback'
 import { prisma } from './repositories/prisma'
 import { graphqlPlayground } from './repositories/graphql-playground'
 import { graphqlYoga } from './repositories/graphql-yoga'
+import { yoga2 } from './repositories/yoga2'
 import { graphqlgen } from './repositories/graphqlgen'
 import { prismaBinding } from './repositories/prisma-binding'
 import { graphqlBinding } from './repositories/graphql-binding';
@@ -12,6 +13,7 @@ const config: Config = {
   'prisma/prisma-admin-feedback': prismaAdminFeedback,
   'prisma/graphql-playground': graphqlPlayground,
   'prisma/graphql-yoga': graphqlYoga,
+  'prisma/yoga2': yoga2,
   'prisma/graphqlgen': graphqlgen,
   'prisma/prisma-binding': prismaBinding,
   'graphql-binding/graphql-binding': graphqlBinding,
