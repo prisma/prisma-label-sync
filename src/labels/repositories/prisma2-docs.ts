d61d246d6b6246d3c56287798ed908f47fcac60d
Matthew Mueller <mattmuelle@gmail.com>
2019-07-02 14:07:37 +0200

    update for prisma 2 projects

--- src/labels/repositories/prisma2-docs.ts
+++ src/labels/repositories/prisma2-docs.ts
@@ -1,10 +1,10 @@
-import { common } from '../common/prisma1'
+import { common } from '../common/prisma2'
 import { reduceAreas } from '../../utils'
 import { RepositoryConfig } from 'label-sync-core'
 
 const areas: string[] = []
 
-export const graphqlBinding: RepositoryConfig = {
+export const prismaDocs: RepositoryConfig = {
   labels: {
     ...common,
     ...reduceAreas(areas),
