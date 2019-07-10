6afec7411dd6bd91dade810e697f82c5244081c0
Harshit Pant <pantharshit00@gmail.com>
2019-07-10 14:55:05 +0530

    feat: add more labels

--- src/labels/common/prisma2.ts
+++ src/labels/common/prisma2.ts
@@ -34,6 +34,16 @@ export const common = {
     description:
       'A new request or an improvement to existing code. Code will need to be added or removed.',
   },
+  'kind/docs': {
+    color: colors.kind,
+    description:
+      'A documentation change is required.',
+  },
+  'kind/discussion': {
+    color: colors.kind,
+    description:
+      'Discussion is required on the suggested code change.',
+  },
   'kind/question': {
     color: colors.kind,
     description: 'Developer asked a question. No code changes required.',
