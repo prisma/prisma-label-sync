248c3883c95b5bd9028f748bb8853e9f38088045
Jan Piotrowski <piotrowski+github@gmail.com>
2019-08-29 17:30:02 +0200

    Fix repo file naming and remove prisma-next (#55)
    * Fix repository file naming
    
    * Update and rename prisma-lift.ts to lift.ts
    
    * Update and rename prisma-photonjs.ts to photonjs.ts
    
    * Update and rename prisma-studio.ts to studio.ts
    
    * Update index.ts
    
    * Update photonjs.ts
    
    * Delete prisma-next.ts
    
    * Update index.ts

--- src/labels/index.ts
+++ src/labels/index.ts
@@ -1,15 +1,17 @@
 import { Config } from 'label-sync-core'
-import { prismaAdminFeedback } from './repositories/prisma-admin-feedback'
+
 import { prisma } from './repositories/prisma'
+import { prismaAdminFeedback } from './repositories/prisma-admin-feedback'
 import { graphqlPlayground } from './repositories/graphql-playground'
 import { graphqlYoga } from './repositories/graphql-yoga'
 import { yoga2 } from './repositories/yoga2'
 import { graphqlgen } from './repositories/graphqlgen'
 import { prismaBinding } from './repositories/prisma-binding'
 import { graphqlBinding } from './repositories/graphql-binding'
-import { prismaStudio } from './repositories/prisma-studio'
-import { prismaPhotonJS } from './repositories/prisma-photonjs'
-import { prismaLift } from './repositories/prisma-lift'
+
+import { studio } from './repositories/studio'
+import { photonjs } from './repositories/photonjs'
+import { lift } from './repositories/lift'
 import { prisma2 } from './repositories/prisma2'
 import { specs } from './repositories/specs'
 import { prisma2Private } from './repositories/prisma2-private'
@@ -17,16 +19,6 @@ import { prismaSdkJs } from './repositories/prisma-sdk-js'
 import { prismaEngine } from './repositories/prisma-engine'
 
 const config: Config = {
-  // prisma 2
-  'prisma/studio': prismaStudio,
-  'prisma/photonjs': prismaPhotonJS,
-  'prisma/lift': prismaLift,
-  'prisma/prisma2': prisma2,
-  'prisma/specs': specs,
-  'prisma/prisma2-private': prisma2Private,
-  'prisma/prisma-sdk-js': prismaSdkJs,
-  'prisma/prisma-engine': prismaEngine,
-
   // prisma 1
   'prisma/prisma': prisma,
   'prisma/prisma-admin-feedback': prismaAdminFeedback,
@@ -36,6 +28,17 @@ const config: Config = {
   'prisma/graphqlgen': graphqlgen,
   'prisma/prisma-binding': prismaBinding,
   'graphql-binding/graphql-binding': graphqlBinding,
+  
+  // prisma 2
+  'prisma/studio': studio,
+  'prisma/photonjs': photonjs,
+  'prisma/lift': lift,
+  'prisma/prisma2': prisma2,
+  'prisma/specs': specs,
+  'prisma/prisma2-private': prisma2Private,
+  'prisma/prisma-sdk-js': prismaSdkJs,
+  'prisma/prisma-engine': prismaEngine,
+
 }
 
 export default config
--- src/labels/repositories/studio.ts
+++ src/labels/repositories/studio.ts
@@ -4,7 +4,7 @@ import { RepositoryConfig } from 'label-sync-core'
 
 const areas: string[] = []
 
-export const prismaNext: RepositoryConfig = {
+export const studio: RepositoryConfig = {
   labels: {
     ...common,
     ...reduceAreas(areas),
