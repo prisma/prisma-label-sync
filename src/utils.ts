1fa083da60e150b77a5f94059cb570e605cbc2e8
Jan Piotrowski <piotrowski+github@gmail.com>
2019-09-06 14:22:31 +0200

    pseudo branch to get CI (#64)
    * feat: add oss labels
    
    * refactor
    
    
    Co-authored-by: Jason Kuhrt <jasonkuhrt@me.com>

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+import { RepositoryConfig, LabelConfig } from 'label-sync-core'
 import { colors } from './labels/common/prisma1'
 
 export function reduceAreas(areas: string[]) {
@@ -7,4 +8,44 @@ export function reduceAreas(areas: string[]) {
       [area]: colors.area,
     }
   }, {})
+}
+export type Index<T> = Record<string, T>
+
+export type RepositoryConfigs = Index<RepositoryConfig>
+
+export type LabelConfigs = Index<LabelConfig>
+
+export type Label = {
+  name: string
+  color: string
+  description?: string
+}
+
+type ReposToLabels = Index<Label[]>
+
+/**
+ * Map the internal repo-to-labels data representation
+ * to what label-sync-core expects.
+ */
+export const toLabelSync = (repos: ReposToLabels): RepositoryConfigs => {
+  return Object.entries(repos).reduce<RepositoryConfigs>(
+    (repositoryConfigs, [repoName, repoLabels]) => {
+      return {
+        ...repositoryConfigs,
+        [repoName]: {
+          labels: repoLabels.reduce<LabelConfigs>((labelConfigs, label) => {
+            return {
+              ...labelConfigs,
+              [label.name]: {
+                // "#" prefix permits vscode extensions to colorize color values
+                color: label.color.replace(/^#(.*)/, '$1'),
+                description: label.description,
+              },
+            }
+          }, {}),
+        },
+      }
+    },
+    {},
+  )
 }
