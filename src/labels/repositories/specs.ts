5b98c2c53711925fc06ec9b9ffad6b03a1d29373
Jan Piotrowski <piotrowski+github@gmail.com>
2019-09-27 11:06:22 +0200

    Prisma2: First iteration of `specs` labels

--- src/labels/repositories/specs.ts
+++ src/labels/repositories/specs.ts
@@ -27,14 +27,17 @@ const areas: string[] = [
 export const specs: RepositoryConfig = {
   labels: {
     // ...common, // no common labels for specs
+    
     // status
     'status/1-needs-info': { color: colors.status },
     'status/2-wait-for-spec': { color: colors.status },
     'status/3-spec-in-progress': { color: colors.status },
+    
     // when
     'when/build-before-ga': { color: colors.priority },
     'when/clarify-before-ga': { color: colors.priority },
     'when/far-future': { color: colors.priority },
+    
     // process
     'process/candidate': { 
       color: colors.process,
@@ -44,6 +47,37 @@ export const specs: RepositoryConfig = {
       color: colors.process,
       description: 'issue earmarked for next milestone',
     },
+    
+    // kind
+    'kind/spec': {
+      color: colors.kind,
+      description: 'Something about an actual spec (file)',
+    },
+    'kind/discussion': {
+      color: colors.kind,
+      description: 'Discussion about if and how something should be written down in a spec',
+    },    
+    
+    // spec
+    'spec/add': { 
+      color: 'd4c5f9',
+      description: 'A new spec should be added',
+    },
+    'spec/change': { 
+      color: 'd4c5f9',
+      description: 'An existing spec should be changed or adapted',
+    },
+    'spec/clarify': { 
+      color: 'd4c5f9',
+      description: 'An existing spec should be clarified',
+    },
+    
+    // special
+    'engineering/blocked': { 
+      color: 'b60205',
+      description: 'This open issue is blocking work in Engineering',
+    },
+    
     ...reduceAreas(areas),
   },
   strict: true,
