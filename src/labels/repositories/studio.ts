0f804e837e68a248f73f640d9b58d5b8bf58ad38
Jan Piotrowski <piotrowski+github@gmail.com>
2019-10-01 18:32:49 +0200

    Prisma2: effort and impact for prisma/studio

--- src/labels/repositories/studio.ts
+++ src/labels/repositories/studio.ts
@@ -1,5 +1,4 @@
 import { common } from '../common/prisma2'
-import { reduceAreas } from '../../utils'
 import { RepositoryConfig } from 'label-sync-core'
 
 const areas: string[] = []
@@ -7,7 +6,27 @@ const areas: string[] = []
 export const studio: RepositoryConfig = {
   labels: {
     ...common,
-    ...reduceAreas(areas),
+    
+    // effort 
+    'effort/high': { 
+      color: '5fd8a4',
+      description: 'This will require significant effort to implement',
+    },
+    'effort/low': { 
+      color: '5fd8a4',
+      description: 'This will not require much effort to implement',
+    },
+    
+    // impact 
+    'impact/high': { 
+      color: '916ff7',
+      description: 'The impact or potential impact is high',
+    },
+    'impact/low': { 
+      color: '916ff7',
+      description: 'The impact or potential impact is low',
+    },
+    
   },
   strict: true,
 }
