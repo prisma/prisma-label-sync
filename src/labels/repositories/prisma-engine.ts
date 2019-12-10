13a3e2d77803689061d708ddbc1b43eb05712d4a
Jan Piotrowski <piotrowski+github@gmail.com>
2019-12-10 17:48:36 +0100

    prisma-engine: component labels (#116)

--- src/labels/repositories/prisma-engine.ts
+++ src/labels/repositories/prisma-engine.ts
@@ -7,6 +7,25 @@ const areas: string[] = []
 export const prismaEngine: RepositoryConfig = {
   labels: {
     ...common,
+    
+    // special
+    'component: migration engine': { 
+      color: 'ffff00', // yellow
+      description: 'Migration Engine',
+    },
+    'component: query engine': { 
+      color: 'ff0000', // red
+      description: 'Query Engine',
+    },
+    'component: introspection engine': { 
+      color: '008000', // green
+      description: 'Introspection Engine',
+    },
+    'component: data model parser': { 
+      color: '0000FF', // blue
+      description: 'Data Model Parser',
+    },
+    
     ...reduceAreas(areas),
   },
   strict: true,
