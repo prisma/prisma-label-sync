2bb8ef92057767e8ad61252a87ad8ee715567c76
Jan Piotrowski <piotrowski+github@gmail.com>
2020-02-10 15:25:51 +0100

    Update index.ts

--- src/labels/index.ts
+++ src/labels/index.ts
@@ -3,16 +3,16 @@ import { Config } from 'label-sync-core'
 import { prisma } from './repositories/prisma'
 import { prismaAdminFeedback } from './repositories/prisma-admin-feedback'
 import { studio } from './repositories/studio'
-import { photonjs } from './repositories/photonjs'
-import { lift } from './repositories/lift'
+import { prismaClientJs } from './repositories/prisma-client-js'
+import { migrate } from './repositories/migrate'
 import { prisma2 } from './repositories/prisma2'
 import { specs } from './repositories/specs'
 import { prisma2Private } from './repositories/prisma2-private'
 import { prismaSdkJs } from './repositories/prisma-sdk-js'
-import { prismaEngine } from './repositories/prisma-engine'
+import { prismaEngines } from './repositories/prisma-engines'
 import { vscodePrisma } from './repositories/vscode-prisma'
 import { prismaExamples } from './repositories/prisma-examples'
-import { photongo } from './repositories/photongo'
+import { prismaClientGo } from './repositories/prisma-client-go'
 import { prisma2DevelopmentEnvironment } from './repositories/prisma2-development-environment'
 import { quaint } from './repositories/quaint'
 
@@ -24,16 +24,16 @@ const config: Config = {
 
   // prisma 2
   'prisma/studio': studio,
-  'prisma/photonjs': photonjs,
-  'prisma/lift': lift,
+  'prisma/prisma-client-js': prismaClientJs,
+  'prisma/migrate': migrate,
   'prisma/prisma2': prisma2,
   'prisma/specs': specs,
   'prisma/prisma2-private': prisma2Private,
   'prisma/prisma-sdk-js': prismaSdkJs,
-  'prisma/prisma-engine': prismaEngine,
+  'prisma/prisma-engines': prismaEngines,
   'prisma/vscode-prisma': vscodePrisma,
   'prisma/prisma-examples': prismaExamples,
-  'prisma/photongo': photongo,
+  'prisma/prisma-client-go': prismaClientGo,
   'prisma/prisma2-development-environment': prisma2DevelopmentEnvironment,
   'prisma/quaint': quaint,
 }
