2d28836f53956c46201627db8d41e39068e3695a
Jan Piotrowski <piotrowski+github@gmail.com>
2020-07-01 17:33:38 +0200

    Open up Studio

--- src/labels/repositories/studio.ts
+++ src/labels/repositories/studio.ts
@@ -9,34 +9,7 @@ delete commonModified['process/next-milestone']
 
 export const studio: RepositoryConfig = {
   labels: {
-    ...commonModified,
-    
-    // effort 
-    'effort/high': { 
-      color: '5fd8a4',
-      description: 'This will require significant effort to implement',
-    },
-    'effort/low': { 
-      color: '5fd8a4',
-      description: 'This will not require much effort to implement',
-    },
-    
-    // impact 
-    'impact/high': { 
-      color: '916ff7',
-      description: 'The impact or potential impact is high',
-    },
-    'impact/low': { 
-      color: '916ff7',
-      description: 'The impact or potential impact is low',
-    },
-    
-    // process
-    'process/needs-design': { 
-      color: colors.process,
-      description: 'This issue needs a concept or design before being actionable',
-    },
-    
+    ...commonModified,    
   },
-  strict: true,
+  strict: false,
 }
