a510fb11edc5d6b25af2912f1bf2a6077c04bc29
Jan Piotrowski <piotrowski+github@gmail.com>
2020-07-08 07:48:18 +0200

    Update studio.ts

--- src/labels/repositories/studio.ts
+++ src/labels/repositories/studio.ts
@@ -3,8 +3,6 @@ import { RepositoryConfig } from 'label-sync-core'
 
 const areas: string[] = []
 
-const common = JSON.parse(JSON.stringify(common))
-
 export const studio: RepositoryConfig = {
   labels: {
     ...common,    
