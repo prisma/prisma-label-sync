3dac748792d8258066b9a12d637e6a3984a944ec
Divyendu Singh <singh@prisma.io>
2020-07-27 15:18:35 +0530

    rename: introspection ci

--- src/labels/repositories/introspection-ci.ts
+++ src/labels/repositories/introspection-ci.ts
@@ -4,7 +4,7 @@ import { RepositoryConfig } from 'label-sync-core'
 
 const areas: string[] = []
 
-export const introspectionEngineOutput: RepositoryConfig = {
+export const introspectionCi: RepositoryConfig = {
   labels: {
     ...common,
     ...reduceAreas(areas),
