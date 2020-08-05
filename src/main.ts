44325b6e4f572c001cc69f6a0e6841e3fcfdb8ce
Jan Piotrowski <piotrowski+github@gmail.com>
2020-08-05 20:03:16 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -14,14 +14,14 @@ import { specs } from "./repositories/specs";
 import { prisma2Private } from "./repositories/prisma2-private";
 import { prismaSdkJs } from "./repositories/prisma-sdk-js";
 import { prismaEngines } from "./repositories/prisma-engines";
-import { vscode } from "./repositories/vscode";
+import { languageTools } from "./repositories/language-tools";
 import { prismaExamples } from "./repositories/prisma-examples";
 import { prismaClientGo } from "./repositories/prisma-client-go";
 import { quaint } from "./repositories/quaint";
 import { prismaTestUtils } from "./repositories/prisma-test-utils";
-import { prisma2E2eTests } from "./repositories/prisma2-e2e-tests";
+import { e2eTests } from "./repositories/e2e-tests";
 import { errorReportingGui } from "./repositories/error-reporting-gui";
-// import { introspectionCi } from "./repositories/introspection-ci";
+import { introspectionCi } from "./repositories/introspection-ci";
 import { reIntrospectionCi } from "./repositories/re-introspection-ci";
 
 labelsync({
@@ -40,14 +40,14 @@ labelsync({
     "prisma2-private": prisma2Private,
     "prisma-sdk-js": prismaSdkJs,
     "prisma-engines": prismaEngines,
-    vscode: vscode,
+    "language-tools": languageTools,
     "prisma-examples": prismaExamples,
     "prisma-client-go": prismaClientGo,
     quaint: quaint,
     "prisma-test-utils": prismaTestUtils,
-    "prisma2-e2e-tests": prisma2E2eTests,
+    "e2e-tests": e2eTests,
     "error-reporting-gui": errorReportingGui,
-    // "introspection-ci": introspectionCi,
+    "introspection-ci": introspectionCi,
     "re-introspection-ci": reIntrospectionCi
   }
 });
