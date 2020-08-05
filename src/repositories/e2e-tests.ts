c9dac4ce727174c66a00f7c4acc3e55cfb38c6a5
Jan Piotrowski <piotrowski+github@gmail.com>
2020-08-05 20:02:00 +0200

    Update and rename prisma2-e2e-tests.ts to e2e-tests.ts

--- src/repositories/e2e-tests.ts
+++ src/repositories/e2e-tests.ts
@@ -1,7 +1,7 @@
 import { repo } from 'label-sync'
 import { common } from '../common/prisma2'
 
-export const prisma2E2eTests = repo({
+export const e2eTests = repo({
   config: {
     removeUnconfiguredLabels: false,
   },
