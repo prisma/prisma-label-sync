18bf61ee48d9b84656debd206069e98e0c850ed1
Jan Piotrowski <piotrowski+github@gmail.com>
2020-11-17 19:03:53 +0100

    Delete prisma-test-utils.ts

--- src/repositories/prisma-test-utils.ts
+++
@@ -1,9 +0,0 @@
-import { common } from '../common/prisma2'
-import { repo } from 'label-sync'
-
-export const prismaTestUtils = repo({
-  config: {
-    removeUnconfiguredLabels: false,
-  },
-  labels: [...common],
-})
