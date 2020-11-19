f7912ad1e616a47d412049e823aaf3878d717b5d
Jan Piotrowski <piotrowski+github@gmail.com>
2020-11-19 14:28:12 +0100

    Delete prisma-sdk-js.ts

--- src/repositories/prisma-sdk-js.ts
+++
@@ -1,9 +0,0 @@
-import { repo } from 'label-sync'
-import { common } from '../common/prisma2'
-
-export const prismaSdkJs = repo({
-  config: {
-    removeUnconfiguredLabels: true,
-  },
-  labels: [...common],
-})
